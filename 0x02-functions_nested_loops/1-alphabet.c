a11dd5ef6e088ff7f5079afce31b2b3e7918e2bc
Shaimaa <Zeinelabdein7754@wcbryanths.org>
2023-09-13 12:35:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,6 +8,11 @@
 void print_alphabet(void)
 
 {
-	char c;
+        char c;
 
-	for (c = 'a'; c
+        for (c = 'a'; c <= 'z'; c++)
+        {       
+                _putchar(c);
+        }       
+        -putchar('\n');
+} 
